360e4c58e445f450be03b3074217d6b1924178a3
PawelPohland <pawel.pohland@gmail.com>
2022-07-18 15:56:06 +0200

    Add strict mode.

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+"use strict";
+
+const incrementFraction = 250;
+
 const counters = document.querySelectorAll(".counter");
 counters.forEach((counter) => {
   counter.textContent = 0;
 
   const updateCounter = () => {
     const target = +counter.getAttribute("data-target");
-    const c = +counter.textContent;
+    const counterValue = +counter.textContent;
 
-    const increment = target / 250;
+    const increment = target / incrementFraction;
 
-    if (c < target) {
-      counter.textContent = `${Math.ceil(c + increment)}`;
+    if (counterValue < target) {
+      counter.textContent = `${Math.ceil(counterValue + increment)}`;
       window.setTimeout(updateCounter, 1);
     } else {
       counter.textContent = target;
